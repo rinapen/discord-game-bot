1aeb4cd8be167583ffb5eda1a74d0cda28904987
xbkv <one.two.o.two@icloud.com>
2025-11-25 14:26:25 +0900

    [remove]: 不要なコメントアウトを削除

--- utils/color.py
+++ utils/color.py
@@ -1,15 +1,11 @@
-"""
-カラーコード定数
-ゲームやUIで使用する色定義
-"""
 from typing import Final
 
-# ゲームカラー
+# ゲーム
 BLACKJACK_COLOR: Final[int] = 0x00704A
 BASE_COLOR_CODE: Final[int] = 0x393a41
 RPS_COLOR: Final[int] = 0x5865F2
 
-# ステータスカラー
+# ステータス
 SUCCESS_COLOR: Final[int] = 0x26ffd4
 ERROR_COLOR: Final[int] = 0xff3d74
 WARNING_COLOR: Final[int] = 0xffa500
